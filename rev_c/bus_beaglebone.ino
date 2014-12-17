99083fb28d5c6162d7f23e502ee90a2cc2394386
Wayne C. Gramlich <Wayne@Gramlich.Net>
2014-12-17 10:30:40 -0800

    More refactoring.

--- rev_c/bus_beaglebone.ino
+++ rev_c/bus_beaglebone.ino
@@ -12,8 +12,8 @@
 
 #define UART1_DISABLED
 
+// Pin defintions for bus_beaglebone:
 #define BUS_STANDBY       3
-#define BUS_NO_ADDRESS   ((UShort)-1)
 #define LED		 13
 
 #define ADDRESS 0x21
@@ -22,134 +22,8 @@
 #define BUFFER_SIZE 16
 
 
+// The *Bus* object is defined here:
 Bus bus;
-
-UShort Bus_Serial__address = BUS_NO_ADDRESS;
-//Byte Bus_Serial__put_index = 0;
-Byte Bus_Serial__put_check_sum = 0;
-Byte Bus_Serial__get_index = 0;
-Byte Bus_Serial__get_check_sum = 0;
-Byte Bus_Serial__get_size = 0;
-//Byte Bus_Serial__put_buffer[BUFFER_SIZE];
-Byte Bus_Serial__get_buffer[BUFFER_SIZE];
-
-Logical Bus_Serial__address_put() {
-  Logical error = (Logical)0;
-
-  // Make sure that the input buffer has nothing:
-  while (bus.can_receive()) {
-    bus.frame_get();
-  }
-
-  Serial.write('R');
-  Serial.print(Bus_Serial__address, HEX);
-
-  // Send the address and get the *address_echo*:
-  bus.frame_put(Bus_Serial__address);
-  UShort address_echo = bus.frame_get();
-  if (Bus_Serial__address != address_echo) {
-    Serial.write('!');
-    error = (Logical)1;
-  }
-
-  // If the address is an upper half address, wait for an acknoledge:
-  if ((Bus_Serial__address & 0x80) == 0) {
-    UShort address_acknowledge = bus.frame_get();
-	
-    Serial.write('r');
-    Serial.print(address_acknowledge, HEX);
-
-    if (address_acknowledge != 0) {
-      Serial.write('!');
-      error = (Logical)1;
-    }
-  }
-  return error;
-}
-
-Logical Bus_Serial__buffer_get() {
-  Logical error = (Logical)0;
-  // Read combined *count_check_sum*:
-  Byte count_check_sum = (Byte)bus.frame_get();
-
-  Serial.write(':');
-  Serial.write('G');
-  Serial.print(count_check_sum, HEX);
-
-  // Split out the *size* and *checksum*:
-  Byte size = (count_check_sum >> 4) & 0xf;
-  Byte check_sum = count_check_sum & 0xf;
-
-  // Read in *size* bytes and compute *computed_check_sume;
-  Byte computed_check_sum = 0;
-  Serial.write(';');
-  for (Byte index = 0; index < size; index++) {
-    Byte byte = (Byte)bus.frame_get();
-    Bus_Serial__get_buffer[index] = byte;
-    computed_check_sum += byte;
-
-    Serial.write('G');
-    Serial.print(byte, HEX);
-  }
-  computed_check_sum += computed_check_sum >> 4;
-  computed_check_sum &= 0xf;
-
-  Bus_Serial__get_index = 0;
-  Bus_Serial__get_size = size;
-
-  if (check_sum != computed_check_sum) {
-    Serial.write('!');
-    error = (Logical)1;
-  }
-  return error;
-}
-
-//Logical Bus_Serial__flush(void) {
-//  Serial.write("{");
-//  Logical error = (Logical)1;
-//  for (Byte retries = 0; error && retries < 5; retries++) {
-//    Serial.write("<");
-//
-//    // Select the address:
-//    error = Bus_Serial__address_put();
-//    if (error) {
-//      Serial.write('!');
-//    } else {
-//      error = Bus_Serial__buffer_put();
-//      if (error) {
-//        Serial.write('@');
-//      } else {
-//        error = Bus_Serial__buffer_get();
-//      }
-//    }
-//
-//    Serial.write(">");
-//  }
-//  Serial.write("}\r\n");
-//  if (error) {
-//    Serial.write('#');
-//  }
-//  return error;
-//}
-
-//void Bus_Serial__byte_put(Byte byte) {
-//  Bus_Serial__put_buffer[Bus_Serial__put_index++] = byte;
-//  Bus_Serial__put_check_sum += byte;
-//}
-
-//Byte Bus_Serial__byte_get(void) {
-//  Serial.write('e');
-//  Bus_Serial__flush();
-//  Serial.write('f');
-//  Byte byte = Bus_Serial__get_buffer[Bus_Serial__get_index++];
-//  Serial.write('g');
-//  return byte;
-//}
-
-//void Bus_Serial__command_byte_put(UShort address, Byte command, Byte byte) {
-//  bus.command_put(address, command);
-//  bus.byte_put(byte);
-//}
 
 // Set the *LED* to the value of *led*:
 void led_set(Logical led) {
@@ -194,11 +68,14 @@ void loop() {
     case TEST_BUS_COMMAND: {
       // Blink the *LED* some:
       Serial.write("[");
-      bus.command_ubyte_put(ADDRESS, LED_PUT, (UByte)HIGH);
+
+      // Set the *LED* to *HIGH* and then wait a little:
+      bus.command_ubyte_put(ADDRESS, LED_PUT, HIGH);
       Logical led_get = bus.command_ubyte_get(ADDRESS, LED_GET);
       led_set(led_get);
       delay(100);
 
+      // Set the *LED* to *LOW* and then wait a little:
       bus.command_ubyte_put(ADDRESS, LED_PUT, LOW);
       led_get = bus.command_ubyte_get(ADDRESS, LED_GET);
       led_set(led_get);
