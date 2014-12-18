a3664cc35f53c79eab6e32bf5c7d7180a04ef984
Wayne C. Gramlich <Wayne@Gramlich.Net>
2014-12-18 15:46:40 -0800

    Got configurator talking over bus again.

--- rev_c/bus_beaglebone.ino
+++ rev_c/bus_beaglebone.ino
@@ -3,12 +3,14 @@
 // This code drives the bus_beagle_bone_black board.
 
 #include "Bus.h"
+#include "Frame_Buffer.h"
 
 #define TEST_BUS_OUTPUT 1
 #define TEST_BUS_ECHO 2
 #define TEST_BUS_COMMAND 3
+#define TEST_BUS_BRIDGE 4
 
-#define TEST TEST_BUS_COMMAND
+#define TEST TEST_BUS_BRIDGE
 
 #define UART1_DISABLED
 
@@ -34,12 +36,148 @@ void led_set(Logical led) {
   }
 }
 
+void led_blink(UShort on, UShort off) {
+  while (1) {
+    led_set((Logical)1);
+    delay(on);
+    led_set((Logical)0);
+    delay(off);
+  }
+}
+
+void bridge_host_to_bus() {
+  Frame_Buffer			bus_frame_in;
+  Frame_Buffer			bus_frame_out;
+  Frame_Buffer			bus_frame_pending;
+  UShort 			echo_suppress = 0xfefe;
+  UShort			high_bits = 0xfefe;
+  Frame_Buffer			host_frame_in;
+  Frame_Buffer			host_frame_out;
+
+  led_set((Logical)1);
+  delay(200);
+  led_set((Logical)0);
+
+  // Just keep looping:
+  while (1) {
+    // Do we have an 8-bit byte to read from the host?:
+    if (Serial.available() > 0 && !host_frame_in.is_full()) {
+      // Receive 8-bit byte and save it into *host_frame_in*:
+      UShort frame = (UShort)Serial.read();
+      host_frame_in.append(frame);
+    }
+
+    // Do we have an 8-bit byte to send to the host?:
+    if (!host_frame_out.is_empty() && Serial.room() >= 0) {
+      // Send *frame* up to host:
+      UShort frame = host_frame_out.lop();
+      Serial.write(frame);
+    }
+
+    // Do we have a 9-bit byte to read from the bus?:
+    if (bus.can_receive() && !bus_frame_in.is_full()) {
+      // Recevie a bus *frame* and save it into *bus_frame_in*:
+      UShort frame = bus.frame_get();
+      bus_frame_in.append(frame);
+    }
+
+    // Is there a pending 9-bit frame to send to bus?:
+    if (!bus_frame_out.is_empty() && bus.can_transmit()) {
+      // Send *frame* out to the bus:
+      UShort frame = bus_frame_out.lop();
+      bus.frame_put(frame);
+
+      // Remember that we want to *echo_suppress* *frame*:
+      echo_suppress = frame;
+    }
+
+    // Forward all non echo suppressed traffic back up to host:
+    if (!bus_frame_in.is_empty()) {
+      // Grab a *frame* from *bus_frame_in*:
+      UShort frame = bus_frame_in.lop();
+      if (echo_suppress == (UShort)0xfefe) {
+	// We are in non-*echo_suppress* mode.  We just forward
+	// the byte up to the host:
+
+	// We need to ensure that the 9th bit is not set:
+	if ((frame & 0xff00) != 0) {
+	  // Since we are the master, the 9th bit should never
+	  // be set.  We indicate our confusion by setting the
+	  // *led* high and masking off the 9th bit:
+	  led_blink(200, 800);
+	}
+
+	// Send *frame* up to host:
+	host_frame_out.append(frame & 0xff);
+      } else {
+	// We are in *echo_suppress* mode:
+
+	// We need to check to ensure that the frame we sent
+	// (=*echo_suppress*) is the same as the one we received
+	// *frame*:
+	if (echo_suppress != frame) {
+	  // Since this should never happen, we indicate our
+	  // confusion by setting *led* to high:
+	  led_blink(500, 500);
+	}
+
+	// Now clear *echo_suppress*:
+        echo_suppress = 0xfefe;
+      }
+    }
+
+    if (!host_frame_in.is_empty() && !bus_frame_out.is_full()) {
+      // Grab *frame* from *host_frame_in*:
+      UShort frame = host_frame_in.lop();
+	    
+      // frames 0xc0 ... 0xc7 are reserved for "special" operations:
+      if ((frame & 0xf8) == 0xc0) {
+	switch (frame & 7) {
+	  case 0:
+	  case 1:
+	  case 2:
+	  case 3:
+	    // 1100 00hh: Set *high_bits*:
+	    // Set *high_bits* to (hh << 7):
+	    high_bits = (frame & 3) << 7;
+	    break;
+	  case 4:
+	    // Do bus discovery scan:
+	    break;
+	  case 5:
+	    // Do bus break:
+	    break;
+	  case 6:
+	    // Reserved:
+	    led_set((Logical)0);
+	    digitalWrite(LED, LOW);
+	    break;
+	  case 7:
+	    // Reserved:
+	    led_set((Logical)1);
+	    break;
+	}
+      } else {
+	if (high_bits == 0xfefe) {
+	  // *high_bits* is not set, just forward *frame* to bus:
+	  bus_frame_out.append(frame);
+	} else {
+	  // *high_bits* is set, forward modified *frame* to bus:
+	  frame = high_bits | (frame & 0x7f);
+	  bus_frame_out.append(frame);
+	  high_bits = 0xfefe;
+	}
+      }
+    }
+  }
+}
+
 // The *setup* routine runs on power up and when you press reset:
 
 void setup() {
   // Initialize the debugging serial port:
   Serial.begin(115200);
-  Serial.write("\r\nbus_beaglebone\r\n");
+  //Serial.write("\r\nbus_beaglebone\r\n");
 
   // Turn the *LED* on:
   pinMode(LED, OUTPUT);
@@ -55,6 +193,7 @@ void setup() {
     case TEST_BUS_OUTPUT:
     case TEST_BUS_ECHO:
     case TEST_BUS_COMMAND:
+    case TEST_BUS_BRIDGE:
       bus.interrupts_disable();
       break;
   }
@@ -65,6 +204,10 @@ void setup() {
 
 void loop() {
   switch (TEST) {
+    case TEST_BUS_BRIDGE: {
+      bridge_host_to_bus();
+      break;
+    }
     case TEST_BUS_COMMAND: {
       // Blink the *LED* some:
       Serial.write("[");
