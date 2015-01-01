deca870f01817da909944af9d7411faec59e54eb
Wayne C. Gramlich <Wayne@Gramlich.Net>
2014-12-31 16:32:25 -0800

    Got bus protocol working better.

--- rev_c/bus_beaglebone.ino
+++ rev_c/bus_beaglebone.ino
@@ -9,8 +9,9 @@
 #define TEST_BUS_ECHO 2
 #define TEST_BUS_COMMAND 3
 #define TEST_BUS_BRIDGE 4
+#define TEST_BUS_LINE 5
 
-#define TEST TEST_BUS_BRIDGE
+#define TEST TEST_BUS_LINE
 
 #define UART0_DISABLED
 #define UART1_DISABLED
@@ -29,7 +30,27 @@ NULL_UART null_uart;
 AVR_UART *bus_uart = &avr_uart1;
 AVR_UART *debug_uart = &avr_uart0;
 AVR_UART *host_uart = &avr_uart0;
-Bus bus((UART *)bus_uart, (UART *)debug_uart);
+Bus bus((UART *)bus_uart, (UART *)host_uart);
+
+typedef struct {
+  Double TargetTicksPerFrame;	// target speed in ticks per frame
+  Integer Encoder;		// encoder count
+  Integer PrevEnc;		// last encoder count
+
+  // Using previous input (PrevInput) instead of PrevError to avoid derivative kick,
+  // see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
+  Short PrevInput;		// last input
+
+  // Using integrated term (ITerm) instead of integrated error (Ierror),
+  // to allow tuning changes,
+  // see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+
+  Short ITerm;			//integrated term
+
+  Integer output;		// last motor setting
+} SetPointInfo;
+
+SetPointInfo leftPID, rightPID;
 
 // Set the *LED* to the value of *led*:
 void led_set(Logical led) {
@@ -49,6 +70,96 @@ void led_blink(UShort on, UShort off) {
   }
 }
 
+static UByte address = 33;
+
+static Short Kp = 20;	// PID Proportional Constant
+static Short Kd = 12;	// PID Differential Constant
+static Short Ki = 0;	// PID Integal Constant
+static Short Ko = 50;	// PID common denOminator 
+static Short const MAX_PWM = 255;
+
+static Logical is_moving = (Logical)0;
+
+void motor_speeds_set(Byte left_speed, Byte right_speed) {
+    is_moving = (Logical)(left_speed != 0) || (right_speed != 0);
+    bus.command_byte_put(address,  9, left_speed);
+    bus.flush();
+    bus.command_byte_put(address, 11, right_speed);
+    bus.flush();
+}
+
+void pid_reset(SetPointInfo *pid){
+   pid->TargetTicksPerFrame = 0.0;
+   //pid->Encoder = encoder;
+   pid->PrevEnc = leftPID.Encoder;
+   pid->output = 0;
+   pid->PrevInput = 0;
+   pid->ITerm = 0;
+}
+
+void do_pid(SetPointInfo *pid) {
+  Integer Perror;
+  Integer output;
+  Short input;
+
+  //Perror = pid->TargetTicksPerFrame - (pid->Encoder - pid->PrevEnc);
+  input = pid->Encoder - pid->PrevEnc;
+  Perror = pid->TargetTicksPerFrame - input;
+
+  // Avoid derivative kick and allow tuning changes, see:
+  //
+  //   http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-derivative-kick/
+  //   http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+
+  //output =
+  // (Kp * Perror + Kd * (Perror - pid->PrevErr) + Ki * pid->Ierror) / Ko;
+  // p->PrevErr = Perror;
+  output = (Kp * Perror - Kd * (input - pid->PrevInput) + pid->ITerm) / Ko;
+  pid->PrevEnc = pid->Encoder;
+
+  output += pid->output;
+  // Accumulate Integral error *or* Limit output.
+  // Stop accumulating when output saturates
+  if (output >= MAX_PWM)
+    output = MAX_PWM;
+  else if (output <= -MAX_PWM)
+    output = -MAX_PWM;
+  else
+    // allow turning changes, see http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-tuning-changes/
+    pid->ITerm += Ki * Perror;
+
+  pid->output = output;
+  pid->PrevInput = input;
+}
+
+void pid_update() {
+  // Read the encoders:
+  leftPID.Encoder = bus.command_integer_get(address, 2);
+  rightPID.Encoder = bus.command_integer_get(address, 4);
+  
+  if (is_moving) {
+    // Do the PID for each motor:
+    //do_pid(&rightPID);
+    //do_pid(&leftPID);
+
+    /* Set the motor speeds accordingly */
+    if (is_moving) {
+      //motor_speeds_set(leftPID.output, rightPID.output);
+    }
+  } else {
+    // If we're not moving there is nothing more to do:
+    // Reset PIDs once, to prevent startup spikes, see
+    //    http://brettbeauregard.com/blog/2011/04/improving-the-beginner%E2%80%99s-pid-initialization/
+    // PrevInput is considered a good proxy to detect
+    // whether reset has already happened
+
+    if (leftPID.PrevInput != 0 || rightPID.PrevInput != 0) {
+	pid_reset(&leftPID);
+	pid_reset(&rightPID);
+    }
+  }
+}
+
 void bridge_host_to_bus() {
   Frame_Buffer			bus_frame_in;
   Frame_Buffer			bus_frame_out;
@@ -57,7 +168,6 @@ void bridge_host_to_bus() {
   UShort			high_bits = 0xfefe;
   Frame_Buffer			host_frame_in;
   Frame_Buffer			host_frame_out;
-
   led_set((Logical)1);
   delay(200);
   led_set((Logical)0);
@@ -180,7 +290,7 @@ void bridge_host_to_bus() {
 
 void setup() {
   // Initialize *avr_uart0* as a debugging port:
-  debug_uart->begin(16000000L, 115200L, (Character *)"8N1");
+  host_uart->begin(16000000L, 115200L, (Character *)"8N1");
 
   // For debugging, dump out UART0 configuration registers:
   //avr_uart0.string_print((Character *)" A:");
@@ -223,23 +333,29 @@ void setup() {
   // Enable/disable interrupts as needed:
   switch (TEST) {
     case TEST_BUS_OUTPUT:
-      debug_uart->string_print((Character *)"\r\nbb_output:\r\n");
-      debug_uart->interrupt_set((Logical)0);
+      host_uart->string_print((Character *)"\r\nbb_output:\r\n");
+      host_uart->interrupt_set((Logical)0);
       bus_uart->interrupt_set((Logical)0);
       break;
     case TEST_BUS_ECHO:
-      debug_uart->string_print((Character *)"\r\nbb_echo:\r\n");
-      debug_uart->interrupt_set((Logical)0);
+      host_uart->string_print((Character *)"\r\nbb_echo:\r\n");
+      host_uart->interrupt_set((Logical)0);
       bus_uart->interrupt_set((Logical)0);
       break;
     case TEST_BUS_COMMAND:
-      debug_uart->string_print((Character *)"\r\nbb_command:\r\n");
-      debug_uart->interrupt_set((Logical)1);
+      host_uart->string_print((Character *)"\r\nbb_command:\r\n");
+      host_uart->interrupt_set((Logical)1);
       bus_uart->interrupt_set((Logical)1);
       break;
     case TEST_BUS_BRIDGE:
       // No announce because we are talking to *host_uart*:
-      //debug_uart->string_print((Character *)"\r\nbb_bridge:\r\n");
+      //host_uart->string_print((Character *)"\r\nbb_bridge:\r\n");
+      host_uart->interrupt_set((Logical)1);
+      bus_uart->interrupt_set((Logical)1);
+      break;
+    case TEST_BUS_LINE:
+      // No announce because we are talking to *host_uart*:
+      host_uart->string_print((Character *)"\r\nbb_line:\r\n");
       host_uart->interrupt_set((Logical)1);
       bus_uart->interrupt_set((Logical)1);
       break;
@@ -251,6 +367,173 @@ void setup() {
 
 void loop() {
   switch (TEST) {
+    case TEST_BUS_LINE: {
+      // Some constants:
+      //static const UInteger PID_RATE = 30;			// Hz.
+      //static const UInteger PID_INTERVAL = 1000 / PID_RATE;	// mSec.
+      //static const UInteger AUTO_STOP_INTERVAL = 2000;		// mSec.
+
+      // Some variables that need to be unchanged through each loop iteration:
+      static UByte address = 33;
+      static Integer arguments[4];
+      static UByte arguments_index = 0;
+      static Character command = ' ';
+      static Logical have_number = (Logical)0;
+      static Logical is_negative = (Logical)0;
+      //static UInteger last_motor_command_time = 0;
+      //static UInteger next_pid = 0;
+      static Integer number = 0;
+
+      // We need to know what time it is *now*:
+      //UInteger now = millis();
+
+      // Process commands one *character* at a time:
+      if (host_uart->can_receive()) {
+	// Grab the next character since we have it:
+	Character character = (Character)host_uart->frame_get();
+
+	// Echo the input:
+	host_uart->frame_put((UShort)character);
+	if (character == '\r') {
+	  host_uart->frame_put((UShort)'\n');
+	}
+
+	// Parse *number* one *character* at a time:
+        if (character == '-') {
+	  // Remember if we have a negative sign:
+	  is_negative = (Logical)1;
+        } else if ('0' <= character && character <= '9') {
+	  // Deal with next decimal digit:
+	  number = number * 10 + (Integer)(character - '0');
+	  have_number = (Logical)1;
+	  //debug_uart->string_print((Text)"num=");
+	  //debug_uart->integer_print((Integer)number);
+	  //debug_uart->string_print((Text)" ");
+	} else if (character == ' ' || character == ':' || character == '\r') {
+	  // Space, colon, and carriage-return terminate a number:
+	  if (have_number) {
+	    // Deal with negative numbers:
+	    if (is_negative) {
+	      number = -number;
+	      is_negative = (Logical)0;
+	    }
+
+	    // Stuff *number* into the *arguments* list:
+	    arguments[arguments_index++] = number;
+	    number = 0;
+	    have_number = (Logical)0;
+
+	    // Reset the *number* parser:
+	    number = 0;
+	    have_number = (Logical)0;
+	    is_negative = (Logical)0;
+	  }
+        } else if ('a' <= character && character <= 'z') {
+	  command = character;
+	}
+
+	// Execute a command if we get '\r':
+	if (character == '\r') {
+	  // Dispatch on *command* character:
+	  switch (command) {
+	    case 'e': {
+	      // Read encoders ("e"):
+	      Integer encoder0 = bus.command_integer_get(address, 2);
+	      Integer encoder1 = bus.command_integer_get(address, 4);
+
+	      host_uart->integer_print(encoder0);
+	      host_uart->string_print((Text)" ");
+	      host_uart->integer_print(encoder1);
+	      host_uart->string_print((Text)"\r\n");
+	      break;
+	    }
+	    case 'm': {
+	      // Set motor speeds ("m left, right"):
+	      Byte left_speed = (Byte)arguments[0];
+	      Byte right_speed = (Byte)arguments[1];
+	      
+	      //bus.command_byte_put(address, 9, left_speed);
+	      //Byte xleft_speed = bus.command_byte_get(address, 8);
+	      //bus.command_byte_put(address, 11, right_speed);
+	      //Byte xright_speed = bus.command_byte_get(address, 10);
+
+	      motor_speeds_set(left_speed, right_speed);
+	      //last_motor_command_time = now;
+
+	      // For debugging:
+	      //debug_uart->integer_print((Integer)left_speed);
+	      //debug_uart->string_print((Text)"==");
+	      //debug_uart->integer_print((Integer)xleft_speed);
+	      //debug_uart->string_print((Text)" ");
+
+	      //debug_uart->integer_print((Integer)right_speed);
+	      //debug_uart->string_print((Text)"==");
+	      //debug_uart->integer_print((Integer)xright_speed);
+	      //debug_uart->string_print((Text)" ");
+
+	      // For PID code:
+	      //leftPID.TargetTicksPerFrame = left_speed;
+	      //rightPID.TargetTicksPerFrame = right_speed;
+	      //motor_speeds_set(left_speed, right_speed);
+
+	      // Print the usual "OK" result:
+	      host_uart->string_print((Text)"OK\r\n");
+	      break;
+	    }
+	    case 'r': {
+	      // Reset encoders ("r"):
+	      bus.command_integer_put(address, 3, 0);
+	      bus.command_integer_put(address, 5, 0);
+
+	      // Print the usual "OK" result:
+	      host_uart->string_print((Text)"OK\r\n");
+	      break;
+	    }
+	    case 'u': {
+	      // Update PID constants ("U Kp Kd Ki Ko");
+	      Kp = arguments[0];
+	      Kd = arguments[1];
+	      Ki = arguments[2];
+	      Ko = arguments[3];
+	      host_uart->string_print((Text)"OK\r\n");
+
+	      // For debugging:
+	      debug_uart->integer_print(Kp);
+	      host_uart->string_print((Text)" ");
+	      debug_uart->integer_print(Kd);
+	      host_uart->string_print((Text)" ");
+	      debug_uart->integer_print(Ki);
+	      host_uart->string_print((Text)" ");
+	      debug_uart->integer_print(Ko);
+
+	      // Print the usual "OK" result:
+	      host_uart->string_print((Text)"OK\r\n");
+	      break;
+	    }
+	    default: {
+	      host_uart->string_print((Text)"Invalid Command\r\n");
+	      break;
+	    }
+	  }
+	  command = '?';
+
+	  // Reset the *arguments_index* for the next command:
+	  arguments_index = 0;
+	}
+      }
+
+      // Do we need to do a PID update cycle?:
+      //if (now > next_pid) {
+	//pid_update();
+        //next_pid += PID_INTERVAL;
+      //}
+
+      // Do we need to shut down the motor?:
+      //if (now - last_motor_command_time > AUTO_STOP_INTERVAL) {
+	//motor_speeds_set(0, 0);
+      //}
+      break;
+    }
     case TEST_BUS_BRIDGE: {
       bridge_host_to_bus();
       break;
@@ -288,18 +571,18 @@ void loop() {
       // Get the resulting *echo_frame* and indicate when it does not match:
       UShort echo_frame = bus.frame_get();
       if ((UShort)character != echo_frame) {
-	debug_uart->string_print((Character *)"!");
+	host_uart->string_print((Character *)"!");
       }
 
       // Wait for the result from the remote module:
       UShort remote_frame = bus.frame_get();
 
-      // Print the *remote_frame* out to *debug_uart*:
-      debug_uart->frame_put(remote_frame);
+      // Print the *remote_frame* out to *host_uart*:
+      host_uart->frame_put(remote_frame);
 
       // Print out any needed CRLF and update to next *character*:
       if (remote_frame == (UShort)'_') {
-	debug_uart->string_print((Character *)"\r\n");
+	host_uart->string_print((Character *)"\r\n");
 	character = '@';
       } else {
 	character += 1;
